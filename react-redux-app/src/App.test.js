ef6f5b20986f6575aecf8daa2ede645693a19cc1
unknown <ce.jaythakkar@gmail.com>
2019-07-31 09:56:33 +0530

    test file

--- react-redux-app/src/App.test.js
+++ react-redux-app/src/App.test.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./containers/App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+// it = test
+
+test("fake test", () => {
+  expect(true).toBeTruthy();
+});
+
+// Uni test
+// it only test one thing
+const add = (x, y) => x + y;
+
+test("should ", () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+//integration
+
+function total(subtotal,total){
+ return add(subtotal,total);
+} 
+
+test('total' , () => {
+  expect(total(5,20)).toBe(25)
+})
